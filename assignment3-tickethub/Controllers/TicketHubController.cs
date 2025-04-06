3e66ca207634cf0a56662e70495fec64516dbe52
Linh Le <lethimylinh.2001@gmail.com>
2025-04-06 15:50:22 -0300

    Add project files.

---
+++ assignment3-tickethub/Controllers/TicketHubController.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Storage.Queues;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assignment3_tickethub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketHubController : ControllerBase
+    {
+        private readonly ILogger<TicketHubController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public TicketHubController(ILogger<TicketHubController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from TicketHub!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(TicketHub tickethub)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+            string queueName = "tickethub";
+            string? connectionString = _configuration["AzureStorageConnectionString"];
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            // serialize an object to json
+            string message = JsonSerializer.Serialize(tickethub);
+
+            // send string message to queue
+            await queueClient.SendMessageAsync(message);
+
+            return Ok("Success- message posted to Storge Queue");
+        }
+
+    }
+}
---
+++ assignment3-tickethub/TicketHub.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace assignment3_tickethub
+{
+    public class TicketHub
+    {
+        [Required]
+        public int ConcertId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Phone]
+        [Required]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [CreditCard(ErrorMessage = "Invalid credit card number.")]
+        public string CreditCard { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/\d{2}$", ErrorMessage = "Invalid expiration date format. Use MM/YY.")]
+        public string Expiration { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"^\d{3}$", ErrorMessage = "Invalid security code. Must be 3 digits.")]
+        public string SecurityCode { get; set; } = string.Empty;
+
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        public string Province { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"(^\d{5}(-\d{4})?$)|(^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$)", ErrorMessage = "Invalid postal code format.")]
+        public string PostalCode { get; set; } = string.Empty;
+
+        [Required]
+        public string Country { get; set; } = string.Empty;
+    }
+}
