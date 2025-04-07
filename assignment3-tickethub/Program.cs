96f3750db0c37682e887ba4d39459e38e7a35fca
Linh Le <lethimylinh.2001@gmail.com>
2025-04-06 23:32:07 -0300

    updating code

--- assignment3-tickethub/Program.cs
+++ assignment3-tickethub/Program.cs
@@ -1,15 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Allow CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()  // Allow all origins (adjust this for more security)
+              .AllowAnyMethod()  // Allow any HTTP method (GET, POST, etc.)
+              .AllowAnyHeader(); // Allow any header
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Add user secrets
+// Add user secrets (for development)
 if (builder.Environment.IsDevelopment())
 {
     builder.Configuration.AddUserSecrets<Program>();
@@ -23,6 +34,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS middleware before UseAuthorization()
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
